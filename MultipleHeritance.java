1dc53c72e90aa6f6bcae14d4713f8adf61958d8b
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-06-03 23:44:45 +0530

    Multiple Inheritance

---
+++ MultipleHeritance.java
@@ -0,0 +1,31 @@
+ 
+interface vehicleone{
+	int  speed=90;
+	public void distance();
+}
+
+interface vehicletwo{
+	int distance=100;
+	public void speed();
+}
+
+class Vehicle  implements vehicleone,vehicletwo{
+	public void distance(){
+		int  distance=speed*100; 
+		System.out.println("distance travelled is "+distance);
+	}
+	public void speed(){
+		int speed=distance/100;
+	}
+}
+
+class MultipleInheritanceUsingInterface{
+	public static void main(String args[]){
+		Vehicle obj = new Vehicle();
+		obj.distance();
+		obj.speed();
+	}
+}
+
+
+
